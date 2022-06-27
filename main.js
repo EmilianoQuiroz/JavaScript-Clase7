d9e75b3056aa6bbe1056c3ceb519af0518010eca
Emiliano Quiroz <santiagoquiroz.dev@gmail.com>
2022-06-27 16:53:42 -0300

    Actualizacion

--- main.js
+++ main.js
@@ -72,4 +72,147 @@ Esto permite definir acciones más dinámicas.
 
 // console.log(duplicado) // [2, 4, 6, 8]
 
-//--- 
+//--- MÉTODOS DE BÚSQUEDA Y TRANSFORMACIÓN ---//
+
+// --- FOR EACH ---//
+/*
+El método forEach() es similar al ejemplo porCadaUno anterior. 
+Itera sobre el array y por cada elemento ejecuta la función que enviemos por parámetro, la cual recibe a su vez el elemento del array que se está recorriendo:
+
+const numeros = [1, 2, 3, 4, 5, 6]
+
+numeros.forEach( (num)=> {
+    console.log(num)
+} )
+*/
+
+//--- FIND ---//
+/*
+El método find() recibe una función de comparación por parámetro. Captura el elemento que se está recorriendo y retorna true o false según la comparación ejecutada. El método retorna el primer elemento que cumpla con esa condición:
+
+const cursos = [
+    {nombre: 'Javascript', precio: 15000},
+    {nombre: 'ReactJS', precio: 22000},
+]
+
+const resultado = cursos.find((el) => el.nombre === "ReactJS")
+const resultado2 = cursos.find((el) => el.nombre === "DW")
+
+console.log(resultado) // {nombre: 'ReactJS', precio: 22000}
+console.log(resultado2) // undefined
+
+Nótese que el find() retorna el primer elemento del array que cumpla con la condición enviada, de ahí que podemos almacenarlo en una variable o usarlo de referencia para otro proceso. Si no hay ninguna coincidencia en el array, el método find retorna undefined.
+*/
+
+//--- FILTER ---//
+/*
+El método filter() recibe, al igual que find(), una función comparadora por parámetro, y retorna un nuevo array con todos los elementos que cumplan esa condición. 
+Si no hay coincidencias, retornará un array vacío.
+
+const cursos = [
+    {nombre: 'Javascript', precio: 15000},
+    {nombre: 'ReactJS', precio: 22000},
+    {nombre: 'AngularJS', precio: 22000},
+    {nombre: 'Desarrollo Web', precio: 16000},
+]
+
+const resultado = cursos.filter((el) => el.nombre.includes('JS'))
+const resultado2 = cursos.filter((el) => el.precio < 14000)
+
+console.log(resultado)
+ [
+    {nombre: 'ReactJS', precio: 22000},
+    {nombre: 'Angular', precio: 22000}
+] 
+
+console.log(resultado2) // []
+*/
+
+//--- SOME ---//
+/*
+console.log( cursos.some((el) => el.nombre == "Desarrollo Web")) 
+// true
+console.log( cursos.some((el) => el.nombre == "VueJS")) 
+// false
+
+El método some() funciona igual que el find() recibiendo una función de búsqueda. En vez de retornar el elemento encontrado, some() retorna true o false según el resultado de la iteración de búsqueda.
+*/
+
+//--- MAP ---//
+/*
+El método map() crea un nuevo array con todos los elementos del original transformados según las operaciones de la función enviada por parámetro. Tiene la misma cantidad de elementos pero los almacenados son el return de la función:
+
+const cursos = [
+    {nombre: 'Javascript', precio: 15000},
+    {nombre: 'ReactJS', precio: 22000},
+    {nombre: 'AngularJS', precio: 22000},
+    {nombre: 'Desarrollo Web', precio: 16000},
+]
+
+const nombres = cursos.map((el) => el.nombre)
+console.log(nombres)
+// [ 'Javascript', 'ReactJS', 'AngularJS', 'Desarrollo Web' ]
+
+En el ejemplo, la función retorna la propiedad nombre de cada elemento y eso es lo que se almacena en el nuevo array nombres. Map() se utiliza mucho para transformación de arrays.
+Si quisiera aumentar el precio de todos los cursos en este ejemplo, puedo mapear y retornar una copia de los elementos con el precio modificado:
+
+const actualizado = cursos.map((el) => {
+    return {
+        nombre: el.nombre,
+        precio: el.precio * 1.25
+    }
+})
+
+console.log(actualizado)
+ [
+     { nombre: 'Javascript', precio: 18750 },
+     { nombre: 'ReactJS', precio: 27500 },
+     { nombre: 'AngularJS', precio: 27500 },
+     { nombre: 'Desarrollo Web', precio: 20000 }
+] 
+
+*/
+
+//--- REDUCE ---//
+/*
+El método reduce() nos permite obtener un único valor tras iterar sobre el array. Funciona como un método que resume el array a un único valor de retorno. 
+
+const numeros = [1, 2, 3, 4, 5, 6]
+const total = numeros.reduce((acumulador, elemento) => acumulador + elemento, 0)
+
+console.log(total) // 21
+
+En este ejemplo, en el acumulador sumamos cada elemento del array y al terminar la iteración nos devuelve ese resultado. El segundo parámetro del reduce, que aquí se ve como 0, es el valor inicial del acumulador.
+*/
+
+//--- SORT ---//
+/*
+El método sort() nos permite reordenar un array según un criterio que definamos. Recibe una función de comparación por parámetro que, a la vez, recibe dos elementos del array. La función retorna un valor numérico (1, -1, 0) que indica qué elemento se posiciona antes o después.
+
+Para ordenar números, basta con restar uno al otro, y el orden indica si será ordenado de forma ascendente o descendente:
+
+const numeros = [ 40, 1, 5, 200 ];
+numeros.sort((a, b) => a - b);  // [ 1, 5, 40, 200 ]
+numeros.sort((a, b) => b - a);  // [ 200, 40, 5, 1 ]
+
+Para ordenar un array por algún string, debemos definir una función comparadora que retorne un valor numérico de referencia, según queramos el orden ascendente o descendente:
+
+const items = [
+    { name: 'Pikachu', price: 21 },
+    { name: 'Charmander', price: 37 },
+    { name: 'Pidgey', price: 45 },
+    { name: 'Squirtle', price: 60 }
+  ]
+items.sort((a, b) => {
+    if (a.name > b.name) {
+        return 1;
+    }
+    if (a.name < b.name) {
+        return -1;
+    }
+    // a es igual a b
+    return 0;
+})
+*/
+
+//011400
